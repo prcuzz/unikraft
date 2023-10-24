236d1b85b01bc59cbdb670a8eb6c1054aaa80323
prcuzz <403124323@qq.com>
2023-10-24 10:35:12 +0800

    20231024 backup

--- lib/unicontainer-exec_hook/exec_hook.c
+++ lib/unicontainer-exec_hook/exec_hook.c
@@ -1,27 +1,41 @@
-// #include "exec_hook.h"
 #include <uk/print.h>
 #include <uk/arch/lcpu.h>
-// #include <arch/regmap_linuxabi.h>
 
-// #define rip		rcx
-// #define rsyscall	orig_rax
-// #define rarg0		rdi
-// #define rarg1		rsi
-// #define rarg2		rdx
-// #define rarg3		r10
-// #define rarg4		r8
-// #define rarg5		r9
+// execve definition:
+//      int execve(const char *filename, char *const argv[], char *const envp[]);
 
-// #define rret0		rax
-// #define rret1		rdx
+// original rip for user programs: rcx
+// system call number: orig_rax
+// 64-bit register parameter passing order: rdi, rsi, rdx, r10, r8, r9
+// return value: rax
+
+// VMCALL nr: rax
+// VMCALL parameter: rbx, rcx, rdx, rsi
 
 #define NULL (0)
+#define VMCALL_UNICONTAINER_EXEC 12
+
+static inline long kvm_hypercall3(unsigned int nr, unsigned long p1,
+                  unsigned long p2, unsigned long p3)
+{
+    long ret;
+    asm volatile("vmcall"
+             : "=a"(ret)
+             : "a"(nr), "b"(p1), "c"(p2), "d"(p3)
+             : "memory");
+    return ret;
+}
 
 int exec_hook(struct __regs *r){
+    unsigned int nr = r->rax;
+    char *filename = r->rdi;
+    char **argv = r->rsi;
+    char **envp = r->rdx;
+
     uk_pr_warn("[unicontainer]ZZZZZZZZZZZZZZZZZZZZZZZZZC\n");
     uk_pr_warn("[unicontainer]syscall number:%lu, arg0:%lu, arg1:%lu, arg2:%lu\n", r->rax, r->rdi, r->rsi, r->rdx);
-    uk_pr_warn("[unicontainer]argv[0]: %s\n", (*(char**)(r->rsi)));
-    uk_pr_warn("[unicontainer]envp[0]: %s\n", (*(char**)(r->rdx)));
+    uk_pr_warn("[unicontainer]argv[0]: %s\n", argv[0]);
+    uk_pr_warn("[unicontainer]envp[0]: %s\n", envp[0]);
     uk_pr_warn("[unicontainer]argv: ");
     unsigned int i = 0;
     while (*((char**)(r->rsi)+i) != 0)
@@ -32,6 +46,7 @@ int exec_hook(struct __regs *r){
     }
     uk_pr_warn("\n");
 
+    kvm_hypercall3(r->rax, r->rdi, r->rsi, r->rdx);
     
     return 0;
 }
