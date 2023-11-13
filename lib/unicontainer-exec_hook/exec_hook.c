4b8330d93e02b89fad67bc12f0d87cc0cdc01585
prcuzz <403124323@qq.com>
2023-11-13 16:52:06 +0800

    Add an address pointer to receive the content returned by the secondary container and call kvm_hypercall4.

--- lib/unicontainer-exec_hook/exec_hook.c
+++ lib/unicontainer-exec_hook/exec_hook.c
@@ -13,9 +13,10 @@
 // VMCALL nr: rax
 // VMCALL parameter: rbx, rcx, rdx, rsi
 
-#define NULL (0)
+#define NULL                        ((void *) 0)
 #define KVM_HC_CLOCK_PAIRING		9
 #define VMCALL_UNICONTAINER_EXEC 	90
+#define DATA_SIZE                   600
 
 static inline long kvm_hypercall2(unsigned int nr, unsigned long p1,
                   unsigned long p2)
@@ -39,18 +40,34 @@ static inline long kvm_hypercall3(unsigned int nr, unsigned long p1,
     return ret;
 }
 
+static inline long kvm_hypercall4(unsigned int nr, unsigned long p1,
+				  unsigned long p2, unsigned long p3,
+				  unsigned long p4)
+{
+	long ret;
+
+	asm volatile("vmcall"
+		     : "=a"(ret)
+		     : "a"(nr), "b"(p1), "c"(p2), "d"(p3), "S"(p4)
+		     : "memory");
+	return ret;
+}
+
 int exec_hook(struct __regs *r){
-    unsigned int nr = r->rax;
-    char *filename = r->rdi;
-    char **argv = r->rsi;
-    char **envp = r->rdx;
+    // unsigned int nr = r->rax;
+    char *filename = (char*)(r->rdi);
+    char **argv = (char**)(r->rsi);
+    char **envp = (char**)(r->rdx);
+    char *ret_context = NULL;
+    unsigned int i = 0;
+    long ret;
+
 
     uk_pr_debug("[unicontainer]ZZZZZZZZZZZZZZZZZZZZZZZZZC\n");
     uk_pr_debug("[unicontainer]syscall number:%lu, arg0:%lu, arg1:%lu, arg2:%lu\n", r->rax, r->rdi, r->rsi, r->rdx);
     uk_pr_debug("[unicontainer]argv[0]: %s\n", argv[0]);
     uk_pr_debug("[unicontainer]envp[0]: %s\n", envp[0]);
     uk_pr_debug("[unicontainer]argv: ");
-    unsigned int i = 0;
     while (*((char**)(r->rsi)+i) != 0)
     {
         // uk_pr_warn("%lu ", (*((char**)(r->rsi)+i)));
@@ -59,10 +76,19 @@ int exec_hook(struct __regs *r){
     }
     uk_pr_debug("\n");
 
-    long ret;
-    // ret = kvm_hypercall2(KVM_HC_CLOCK_PAIRING, 0, 0);
-    ret = kvm_hypercall3(VMCALL_UNICONTAINER_EXEC, r->rdi, r->rsi, r->rdx);
+    ret_context = (char*)malloc(DATA_SIZE);
+    if(NULL == ret_context){
+        uk_pr_warn("[unicontainer]malloc() fail in exec_hook()\n");
+        UK_CRASH("[unicontainer]malloc() fail, exec_hook() Exiting\n");
+    }
+
+    ret = kvm_hypercall4(VMCALL_UNICONTAINER_EXEC, r->rdi, r->rsi, r->rdx, (unsigned long)ret_context);
     uk_pr_debug("[unicontainer]return value from kvm vmcall: %ld\n", ret);
+    uk_pr_debug("[unicontainer]return context from kvm vmcall: %s\n", ret_context);
+    write(1, ret_context, strlen(ret_context));
+    write(1, "\n", strlen("\n"));
+
+    free(ret_context);
 
     UK_CRASH("[unicontainer]exec_hook() Exiting\n");
     return 0;
